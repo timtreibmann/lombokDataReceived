6a7023ab65162eed2ac22cb16c1643adf0952dfc
tim <s58871@beuth-hochschule.de>
2017-08-30 16:48:29 +0200

    Added @DataReceived Annotation Added Handler for @DataReceived Annotation Added CTC_AND in Javac.java to have access to an && operator
    Added @PrintAST for Javac
    Modified import in HanldePrintAST.java for @PrintAST Annotation import

---
+++ src/core/lombok/DataReceived.java
@@ -0,0 +1,16 @@
+package lombok;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface DataReceived {
+
+    String value() default "VALID";
+    String controller() default "Mavenproject1Controller";
+
+}
+
